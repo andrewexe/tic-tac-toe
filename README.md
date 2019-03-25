ae5aa3a59add061acbca5c4cccef5e32ef01823a
William Huang <whuang04@gmail.com>
2019-03-24 22:40:06 -0400

    updated some files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tic-tac-toe
 tic tac toe game
+
+# Requires Java 8 to run this
+
--- src/andrew/games/TicTacToe.java
+++ src/andrew/games/TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("Tic Tac Toe game");
-
+		System.out.println("Cool game!");
 	}
 
 }
